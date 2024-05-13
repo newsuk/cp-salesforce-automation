ad971089d1c6b6361aced97719eba5a0416167ab
Krishnan <bkrishnan@NTS251953MC.local>
2024-05-13 13:01:59 +0530

    Salesforce POC

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,7 @@ const { defineConfig } = require("cypress");
 const cucumber = require("cypress-cucumber-preprocessor").default;
 module.exports = defineConfig({
   e2e: {
-    "baseUrl": "https://news-uk--uat23.sandbox.lightning.force.com",
-    "chromeWebSecurity": false,
+    "baseUrl": "https://test.salesforce.com/",
     specPattern: "**/*.feature",
     setupNodeEvents(on, config) {
       on("file:preprocessor", cucumber());
